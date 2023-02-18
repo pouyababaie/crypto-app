f9d274e029edd65f654217a1593eab891ed3ebf2
p.babaie <pouyababaie123@gmail.com>
2023-02-18 04:18:27 +0330

    bootstrap downgraded

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, SideNavComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   exports: [HeaderComponent, SideNavComponent, FooterComponent],
 })
 export class LayoutModule {}
--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
+  navigation = [0, 0, 0];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
